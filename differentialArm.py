f043c7dbe128e371c8cd65103b63e2cede20db31
Efe Çalışkan <efecaliskan08@gmail.com>
2025-07-27 21:45:11 +0300

    Create differentialArm.py

---
+++ differentialArm.py
@@ -0,0 +1,309 @@
+import motor
+import motor_pair
+import runloop
+from hub import motion_sensor, port
+import utime
+import color_sensor
+
+
+def getGyro():
+    return -1*motion_sensor.tilt_angles()[0]
+
+def printGyro():
+    print(getGyro())
+
+#def raspi(c):
+#    return input(f"{c}:")
+
+def adamGibiDon(angle:int, turnDir="f"):
+    init_gyro = getGyro()
+    mot1 = None
+    mot2 = None
+    if turnDir == "f" or turnDir=="F":
+        mot1=port.F # sol motor
+        mot2=port.B # sag motor
+    else:
+        mot1=port.B # sag motor
+        mot2=port.F # sol motor
+
+    oran_ti1 = -1*int((angle-init_gyro) * 8/11)
+    oran_ti2 = -1*int((angle-init_gyro) * 6/11)
+    oran_ti3 = -1*int(4/11 * (angle-init_gyro))
+
+    if angle>init_gyro: #angle>0
+        while angle >= getGyro():
+            if 1/7 * angle > getGyro():
+                motor.run(mot1, oran_ti3)
+            elif 2/7 * angle > getGyro():
+                motor.run(mot1, oran_ti1)
+            elif 4/7 * angle > getGyro():
+                motor.run(mot1, oran_ti2)
+            else:
+                motor.run(mot1, -280)
+
+        motor.stop(mot1, stop=motor.HOLD)
+    else:
+        while angle <= getGyro():#angle+init_gyro
+            if 1/7 * angle < getGyro():
+                motor.run(mot2, oran_ti3)
+            elif 2/7 * angle < getGyro():
+                motor.run(mot2, oran_ti1)
+            elif 4/7 * angle < getGyro():
+                motor.run(mot2, oran_ti2)
+            else:
+                motor.run(mot2, 280)
+
+    motor_pair.stop(motor_pair.PAIR_1, stop=motor.HOLD)
+    utime.sleep_ms(40)
+
+def moveWithGyro(velocity:int, angle:int, duration:int, ST = motor.HOLD):
+    utime.sleep_ms(40)
+    motor_pair.stop(motor_pair.PAIR_1, stop=ST)
+    start_time = utime.ticks_ms()
+    while utime.ticks_diff(utime.ticks_ms(), start_time) < duration:
+        adj = int(angle-getGyro())
+        motor_pair.move_tank(motor_pair.PAIR_1, velocity + adj, velocity - adj)
+
+
+    motor_pair.stop(motor_pair.PAIR_1,stop=ST)
+    utime.sleep_ms(30)
+
+def twoWheelTurn(angle:int):
+    motor_pair.stop(motor_pair.PAIR_1,stop=motor.HOLD)
+    utime.sleep_ms(100)
+    init_gyro = getGyro()
+    oran_ti1 = -1*int((angle-init_gyro) * 4/11)
+    oran_ti2 = -1*int((angle-init_gyro) * 3/11)
+    oran_ti3 = -1*int(2/11 * (angle-init_gyro))
+    if angle>init_gyro:
+        while angle>= getGyro():
+            if 1/7 * angle > getGyro():
+                motor_pair.move_tank(motor_pair.PAIR_1, -1*oran_ti3, oran_ti3)
+            elif 4/7 * angle > getGyro():
+                motor_pair.move_tank(motor_pair.PAIR_1, -1*oran_ti1, oran_ti1)
+            elif 5/7 * angle > getGyro():
+                motor_pair.move_tank(motor_pair.PAIR_1, -1*oran_ti2, oran_ti2)
+            else:
+                motor_pair.move_tank(motor_pair.PAIR_1, 140, -1*140)
+
+        motor_pair.stop(motor_pair.PAIR_1,stop=motor.HOLD)
+        return
+    else:
+        while angle <= getGyro():
+            if 1/7 * angle < getGyro():
+                motor_pair.move_tank(motor_pair.PAIR_1, -1*oran_ti3, oran_ti3)
+            elif 4/7 * angle < getGyro():
+                motor_pair.move_tank(motor_pair.PAIR_1, -1*oran_ti3, oran_ti3)
+            elif 5/7 * angle < getGyro():
+                motor_pair.move_tank(motor_pair.PAIR_1, -1*oran_ti3, oran_ti3)
+            else:
+                motor_pair.move_tank(motor_pair.PAIR_1, -1*140, 140)
+
+        motor_pair.stop(motor_pair.PAIR_1,stop=motor.HOLD)
+        return
+
+def gyroRun(velocity:int, angle:int):
+    adj = int(angle-getGyro())
+    motor_pair.move_tank(motor_pair.PAIR_1, velocity + adj, velocity - adj)
+
+def fastfastTurn(angle:int, turnDir="f"):
+    # motor.run(port.A, 1000) positive
+    motor_pair.stop(motor_pair.PAIR_1,stop=motor.HOLD)
+    utime.sleep_ms(30)
+    init_gyro = getGyro()
+    if turnDir == "f" or turnDir=="F":
+        mot1=port.F # sol motor
+        mot2=port.B # sag motor
+    else:
+        mot1=port.B # sag motor
+        mot2=port.F # sol motor
+    #angle<init_gyro
+    oran_ti1 = -1*int((angle-init_gyro) * 9/10)
+    oran_ti2 = -1*int((angle-init_gyro) * 7/10)
+    oran_ti3 = -1*int(5/10 * (angle-init_gyro))
+
+    if angle>init_gyro: #angle>0
+        while angle >= getGyro():
+            if 1/7 * angle > getGyro():
+                motor.run(mot1, oran_ti3)
+            elif 4/7 * angle > getGyro():
+                motor.run(mot1, oran_ti1)
+            elif 5/7 * angle > getGyro():
+                motor.run(mot1, oran_ti2)
+            else:
+                motor.run(mot1, -350)
+
+        motor.stop(mot1, stop=motor.HOLD)
+    else:
+        while angle <= getGyro():#angle+init_gyro
+            if 1/7 * angle < getGyro():
+                motor.run(mot2, oran_ti3)
+            elif 4/7 * angle < getGyro():
+                motor.run(mot2, oran_ti1)
+            elif 5/7 * angle < getGyro():
+                motor.run(mot2, oran_ti2)
+            else:
+                motor.run(mot2, 350)
+
+        motor.stop(mot2, stop=motor.HOLD)
+
+    motor_pair.stop(motor_pair.PAIR_1, stop=motor.HOLD)
+    utime.sleep_ms(80)
+
+def twoWheelSlow(angle):
+    motor_pair.stop(motor_pair.PAIR_1,stop=motor.HOLD)
+    utime.sleep_ms(100)
+    init_gyro = getGyro()
+    oran_ti1 = -1*int((angle-init_gyro) * 6/22)
+    oran_ti2 = -1*int((angle-init_gyro) * 4/22)
+    oran_ti3 = -1*int(3/22 * (angle-init_gyro))
+    if angle>init_gyro:
+        while angle>= getGyro():
+            if 1/7 * angle > getGyro():
+                motor_pair.move_tank(motor_pair.PAIR_1, -1*oran_ti3, oran_ti3)
+            elif 4/7 * angle > getGyro():
+                motor_pair.move_tank(motor_pair.PAIR_1, -1*oran_ti1, oran_ti1)
+            elif 5/7 * angle > getGyro():
+                motor_pair.move_tank(motor_pair.PAIR_1, -1*oran_ti2, oran_ti2)
+            else:
+                motor_pair.move_tank(motor_pair.PAIR_1, 140, -1*140)
+
+        motor_pair.stop(motor_pair.PAIR_1,stop=motor.HOLD)
+
+    else:
+        while angle <= getGyro():
+            if 1/7 * angle < getGyro():
+                motor_pair.move_tank(motor_pair.PAIR_1, -1*oran_ti3, oran_ti3)
+            elif 4/7 * angle < getGyro():
+                motor_pair.move_tank(motor_pair.PAIR_1, -1*oran_ti3, oran_ti3)
+            elif 5/7 * angle < getGyro():
+                motor_pair.move_tank(motor_pair.PAIR_1, -1*oran_ti3, oran_ti3)
+            else:
+                motor_pair.move_tank(motor_pair.PAIR_1, -1*140, 140)
+
+        motor_pair.stop(motor_pair.PAIR_1,stop=motor.HOLD)
+
+    utime.sleep_ms(50)
+
+def gyroTurn(angle:int, spid="f",turnDir="f", ST=motor.HOLD):
+    # motor.run(port.A, 1000) positive
+    #motor_pair.stop(motor_pair.PAIR_1,stop=ST)
+    utime.sleep_ms(30)
+    init_gyro = getGyro()
+    if turnDir == "f" or turnDir=="F":
+        mot1=port.F # sol motor
+        mot2=port.B # sag motor
+    else:
+        mot1=port.B # sag motor
+        mot2=port.F # sol motor
+    if spid=="s" or spid=="S":
+        #angle<init_gyro
+        oran_ti1 = -1*int((angle-init_gyro)* 8/11)
+        oran_ti2 = -1*int((angle-init_gyro) * 6/11)
+        oran_ti3 = -1*int(4/11 * (angle-init_gyro))
+
+    else:
+        #angle<init_gyro
+        oran_ti1 = -1*int((angle-init_gyro) * 4/5)
+        oran_ti2 = -1*int((angle-init_gyro) * 3/5)
+        oran_ti3 = -1*int(2/5 * (angle-init_gyro))
+    if angle>init_gyro: #angle>0
+        while angle >= getGyro(): # angle+init_gyro
+            if 1/7 * angle > getGyro():
+                motor.run(mot1, oran_ti3)
+            elif 4/7 * angle > getGyro():
+                motor.run(mot1, oran_ti1)
+            elif 5/7 * angle > getGyro():
+                motor.run(mot1, oran_ti2)
+            else:
+                motor.run(mot1, -280)
+
+        motor.stop(mot1, stop=motor.HOLD)
+    else:
+        while angle <= getGyro():
+
+            if 1/7 * angle < getGyro():
+                motor.run(mot2, oran_ti3)
+            elif 4/7 * angle < getGyro():
+                motor.run(mot2, oran_ti1)
+            elif 5/7 * angle < getGyro():
+                motor.run(mot2, oran_ti2)
+            else:
+                motor.run(mot2, 280)
+
+        motor.stop(mot2, stop=motor.HOLD)
+    motor_pair.stop(motor_pair.PAIR_1, stop=motor.HOLD)
+    utime.sleep_ms(140)
+
+def checkColor(minimum:int, maximum:int):
+    W = (color_sensor.rgbi(port.A)[0] + color_sensor.rgbi(port.A)[1] + color_sensor.rgbi(port.A)[2])/3
+    if W > minimum and W < maximum:
+        return True
+    else:
+        return False
+
+
+class DifferentialArms():
+    ARM1 = 1 # Arm with no load
+    ARM2 = 2 # Arm with load
+    OPEN = 0
+
+    def __init__(self, port, one_rotation_angle=64, close_direction=1,speed=400,start_position=0):
+        """
+        port: motor port
+        one_rotation_angle: the required motor rotation for one arm to move
+        start: starting position of the arms
+                0: Both arms open
+                ARM1: ARM1 is closed
+                ARM2: ARM2 is cloesed (means ARM1 is also closed)
+        """
+
+        self._port = port
+        self._one_rotation_angle = one_rotation_angle
+        self.close_direction = close_direction
+        self.speed=speed
+        start_angle = int((2.5 if start_position==2 else 1 if start_position==1 else 0)*one_rotation_angle*close_direction)
+        print(start_angle)
+        motor.reset_relative_position(self._port,start_angle)
+
+    async def _moveArm(self,n,direction,speed):
+        await motor.run_to_relative_position(self._port, int(self._one_rotation_angle * n * direction), speed)
+
+    async def set_closed_arms(self, arm, speed=400):
+        if arm==self.ARM1:
+            await self._moveArm(1,self.close_direction,speed)
+
+        elif arm==self.ARM2:
+            await self._moveArm(2.5,self.close_direction,speed)
+
+        elif arm==0:
+            await self._moveArm(0,self.close_direction,speed)
+        # await motor.run_to_relative_position(self._port,n*self._one_rotation_angle, 400)
+
+    async def set_partial_closure(self, n, both_arms=True,speed=400):
+        await diff_arms._moveArm(n*(2.5 if both_arms else 1),self.close_direction,speed=speed)
+
+diff_arms = DifferentialArms(port.B,close_direction=1,start_position=2)
+
+
+async def main():
+    motion_sensor.reset_yaw(0)
+    motor_pair.pair(motor_pair.PAIR_1,port.A,port.C)
+    
+    await diff_arms.set_closed_arms(0)
+    
+    moveWithGyro(-500,0,450)
+    
+    await diff_arms.set_partial_closure(0.9)
+
+    runloop.sleep_ms(1000)
+    moveWithGyro(-200,0,850)
+
+    await diff_arms.set_closed_arms(0)
+
+    moveWithGyro(300,0,650)
+
+    await diff_arms.set_closed_arms(2,speed=200)
+
+
+runloop.run(main())
